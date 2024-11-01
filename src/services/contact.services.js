9e74b616157556d5ccfb30e59963267508061222
nguyenvanlap1 <nguyenvanlap3965@gmail.com>
2024-11-01 16:29:10 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/services/contact.services.js
@@ -0,0 +1,33 @@
+import createApiClient from './api.services';
+
+class ContactService {
+    constructor(baseUrl= '/api/contacts') {
+        this.api = createApiClient(baseUrl);
+    }
+
+    async getAll() {
+        return (await this.api.get("/")).data;
+    }
+
+    async create(data) {
+        rerturn (await this.api.post("/", data)).data;
+    }
+
+    async deleteAll() {
+        return (await this.api.delete("/")).data;
+    }
+
+    async get(id) {
+        retrun (await this.api.get(`/${id}`)).data;
+    }
+
+    async update(id, data) {
+        return (await this.api.put(`/${id}`, data)).data;
+    }
+
+    async delete(id) {
+        return (await this.api.delete(`/${id}`, data)).data;
+    }
+}
+
+export default new ContactService();
